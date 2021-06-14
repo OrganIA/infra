8f1e77dde4027b3bf18b2268e2f3e918794f746d
Tina <tina@organia-prod.u2bahq2gbxderjlb31zfv2icff.parx.internal.cloudapp.net>
2021-06-14 11:15:51 +0000

    Ignore SQL logs

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,6 +11,6 @@ compose build
 compose down -t 1
 compose up -d
 if [ "$ENV" != 'prod' ]; then
-	FORCE_LOGIN=1 compose run --rm -p 9000:10080 -d backend
+	DISCORD_LOGS= FORCE_LOGIN=1 compose run --rm -p 9000:10080 -d backend
 fi
 compose logs -f
